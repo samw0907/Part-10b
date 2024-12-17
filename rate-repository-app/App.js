55c2524929eff6f95f8694fb46197715fbcb46ef
Sam <swilliamson_0907@outlook.com>
2024-12-17 19:13:53 +0200

    Frontend features and styling, primarily AppBar ans SignIn

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
 
-export default function App() {
+import { NativeRouter } from 'react-router-native'
+
+import Main from './src/components/Main'
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Some default text</Text>
+    <>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
       <StatusBar style="auto" />
-    </View>
-  );
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
